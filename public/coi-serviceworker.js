95d2d35db4ed07c9e4a38a15f7861b5e37f01c0d
Nick85Rn <markettiamoeprogrammiamo@gmail.com>
2025-12-25 15:36:14 +0100

    aggiornato coi-serviceworker.js

--- public/coi-serviceworker.js
+++ public/coi-serviceworker.js
@@ -58,7 +58,7 @@ if (typeof window === 'undefined') {
     if (navigator.serviceWorker) {
       navigator.serviceWorker.register(window.document.currentScript.src).then(
         (registration) => {
-            console.log("COI Service Worker registrato per sbloccare FFmpeg");
+            console.log("COI Service Worker registrato correttamente");
             registration.addEventListener("updatefound", () => {
                 window.location.reload();
             });
